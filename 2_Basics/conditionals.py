e12467369f4b02220f1211bcccb9101423056f7e
DarthPlagueisTheWise <ved.chikarmane@gmail.com>
2025-04-27 13:44:44 +0530

    Added conditionals.py (if-else logic)

---
+++ 2_Basics/conditionals.py
@@ -0,0 +1,10 @@
+# Making decisions with if-else
+
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("You got an A!")
+elif score >= 75:
+    print("You got a B!")
+else:
+    print("You need more practice.")
