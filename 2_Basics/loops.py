63b479a61ca464430d20db801a4cbd1f9cf184e2
DarthPlagueisTheWise <ved.chikarmane@gmail.com>
2025-04-28 12:42:59 +0530

    Added loops.py (for loop and while loop)

---
+++ 2_Basics/loops.py
@@ -0,0 +1,11 @@
+# Understanding loops
+
+# For loop example
+for i in range(5):
+    print("For Loop iteration:", i)
+
+# While loop example
+count = 0
+while count < 5:
+    print("While Loop count:", count)
+    count += 1
