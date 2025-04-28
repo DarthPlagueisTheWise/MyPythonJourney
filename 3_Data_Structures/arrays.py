1eb180ccecd0cb1f73892f5b783c1d39428eaf6e
DarthPlagueisTheWise <ved.chikarmane@gmail.com>
2025-04-28 12:44:03 +0530

    Added arrays.py (working with lists)

---
+++ 3_Data_Structures/arrays.py
@@ -0,0 +1,11 @@
+# Introduction to lists (arrays)
+
+fruits = ["apple", "banana", "cherry"]
+
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+
+# Adding a new fruit
+fruits.append("mango")
+print("Updated fruits list:", fruits)
+
