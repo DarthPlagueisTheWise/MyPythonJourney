4f33de2518ae9ddca8144e0832bafe1562877e6e
DarthPlagueisTheWise <ved.chikarmane@gmail.com>
2025-05-02 17:03:41 +0530

    Added factorial_recursive.py (intro to recursion)

---
+++ 6_Recursion/factorial_recursive.py
@@ -0,0 +1,12 @@
+# Factorial using recursion
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Example usage
+num = 5
+print("Factorial of", num, "is", factorial(num))
+
